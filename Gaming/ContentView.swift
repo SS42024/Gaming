dc2c70564ab79500778a52cbca60d17c3f0867ed
Shailesh Sharavana <shailesh.sharavana@gmail.com>
2024-11-05 13:11:04 -0800

    Replacing To Unknow

--- Gaming/ContentView.swift
+++ Gaming/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hi Shailesh!")
+            Text("Hi Unknown!")
         }
         .padding()
     }
