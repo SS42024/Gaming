24458d213d578c77723b34f861f81b6a4b255c63
Shailesh Sharavana <shailesh.sharavana@gmail.com>
2024-11-05 12:59:51 -0800

    Initial Commit

---
+++ Gaming/GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamingApp.swift
+//  Gaming
+//
+//  Created by shailesh sharavana on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GamingTests/GamingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GamingTests.swift
+//  GamingTests
+//
+//  Created by shailesh sharavana on 11/5/24.
+//
+
+import Testing
+@testable import Gaming
+
+struct GamingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
